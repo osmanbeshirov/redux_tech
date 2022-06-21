4264d8a7b216ffaed25954754f06049c38f91577
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-22 02:58:04 +0400

    useselector added to redux

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 const Child = () => {
+
+    const todo  = useSelector((state) => state)
+
+    console.log(todo)
+
+
     return (
         <div>Child</div>
     )
---
+++ src/redux/reducers/todoReducer.js
@@ -0,0 +1,7 @@
+
+const initial = [1,2,3]
+
+
+export const todoReducer = (state = initial, action) => {
+    return state;
+}
