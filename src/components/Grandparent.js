52bfc1349af3dff63c9ae6e291d2ec711427bdba
osmanbeshirov <osman.beshirov@gmail.com>
2022-06-22 02:18:29 +0400

    basics components added

---
+++ src/components/Grandparent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Parent from './Parent'
+
+const Grandparent = () => {
+    return (
+        <div><Parent/></div>
+    )
+}
+
+export default Grandparent;
---
+++ src/components/Parent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import Child from './Child'
+
+const Parent = () => {
+    return (
+        <div><Child/></div>
+    )
+}
+
+export default Parent;
